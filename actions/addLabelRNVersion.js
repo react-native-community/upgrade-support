73ebb0d4f3a53c47c3c7267b5ccf437c234f4d3f
Lucas Bento <lucas.bsilva@outlook.com>
2020-01-09 10:31:03 +0100

    Add GitHub action to automatically get the RN version from issue body

---
+++ actions/addLabelRNVersion.js
@@ -0,0 +1,96 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const md2json = require("md-2-json");
+const findVersions = require("find-versions");
+
+// Look for a version on the issue body
+const searchForVersion = upgradingVersionSection => {
+  const versions = findVersions(upgradingVersionSection, { loose: true });
+
+  if (versions.length === 0) {
+    return;
+  }
+
+  return versions[0].replace(/\.0+$/m, "");
+};
+
+(async () => {
+  const { issue } = github.context;
+
+  const client = new github.GitHub(
+    core.getInput("github-token", { required: true })
+  );
+
+  // This fetches the issue again as it can have different data after running the other actions
+  const { data: updatedIssue } = await client.issues.get({
+    owner: issue.owner,
+    repo: issue.repo,
+    issue_number: issue.number
+  });
+
+  if (updatedIssue.state === "closed") {
+    // Do nothing if the issue has been closed
+    return;
+  }
+
+  const parsedIssueBodyMarkdown = md2json.parse(
+    github.context.payload.issue.body
+  );
+
+  try {
+    const { raw: upgradingVersionSection } = parsedIssueBodyMarkdown[
+      "Environment"
+    ]["Upgrading version"];
+
+    const version = searchForVersion(upgradingVersionSection);
+
+    const { data: labels } = await client.issues.listLabelsOnIssue({
+      owner: issue.owner,
+      repo: issue.repo,
+      issue_number: issue.number
+    });
+
+    await Promise.all(
+      labels.map(async ({ name }) => {
+        // Figure out how to kill the code here as the label is already defined
+        if (name === version) {
+          return;
+        }
+
+        if (searchForVersion(name)) {
+          return await client.issues.removeLabel({
+            owner: issue.owner,
+            repo: issue.repo,
+            issue_number: issue.number,
+            name
+          });
+        }
+      })
+    );
+
+    if (!version) {
+      // No version found, do nothing
+      return;
+    }
+
+    try {
+      await client.issues.getLabel({
+        owner: issue.owner,
+        repo: issue.repo,
+        name: version
+      });
+
+      await client.issues.addLabels({
+        owner: issue.owner,
+        repo: issue.repo,
+        issue_number: issue.number,
+        labels: [version]
+      });
+    } catch (_error) {
+      // Label does not exist, do nothing
+      return;
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
