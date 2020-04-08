667cb28f4c77e49c1f3f8aa41dbf41091cf20701
Lucas Bento <lucas.bsilva@outlook.com>
2020-04-08 20:07:29 +0200

    Add label for when bot is not able to identify a release version

--- actions/addLabelRNVersion/index.js
+++ actions/addLabelRNVersion/index.js
@@ -4,6 +4,8 @@ const semverRcompare = require("semver/functions/rcompare");
 const md2json = require("md-2-json");
 const findVersions = require("find-versions");
 
+const labelForNoVersion = "🙁 No upgrading version specified";
+
 // Look for a version on the issue body
 const searchForVersion = (upgradingVersionSection) => {
   const versions = findVersions(upgradingVersionSection, { loose: true });
@@ -49,27 +51,29 @@ const searchForVersion = (upgradingVersionSection) => {
 
     const version = searchForVersion(upgradingVersionSection);
 
-    if (!version) {
-      core.debug("No version found.");
+    core.debug(version ? `Found version: ${version}` : "No version found.");
 
-      return;
-    }
-
-    core.debug(`Found version: ${version}`);
-
+    // Get all the labels in the issue
     const { data: labels } = await client.issues.listLabelsOnIssue({
       owner: issue.owner,
       repo: issue.repo,
       issue_number: issue.number,
     });
 
+    // Loop through them
     await Promise.all(
       labels.map(async ({ name }) => {
-        if (name === version) {
+        // Check that the version has been already added, if so, throw
+        if (version && name === version) {
           throw new Error("Version already added to issue.");
         }
 
-        if (searchForVersion(name)) {
+        const isLabelAVersion = !!searchForVersion(name);
+        const isLabelForNoVersion = name === labelForNoVersion;
+
+        // If there's a label that's not the version found and IS a release version
+        // or the `labelForNoVersion`, remove it
+        if (isLabelAVersion || (version && isLabelForNoVersion)) {
           core.debug("Removing outdated version from issue");
 
           return await client.issues.removeLabel({
@@ -82,22 +86,25 @@ const searchForVersion = (upgradingVersionSection) => {
       })
     );
 
+    const labelToAdd = version || labelForNoVersion;
+
     try {
+      // Make sure that the label to be added exists
       await client.issues.getLabel({
         owner: issue.owner,
         repo: issue.repo,
-        name: version,
+        name: labelToAdd,
       });
 
       await client.issues.addLabels({
         owner: issue.owner,
         repo: issue.repo,
         issue_number: issue.number,
-        labels: [version],
+        labels: [labelToAdd],
       });
     } catch (_error) {
-      // Label does not exist, do nothing
-      core.debug("Label with version does not exist");
+      // Label does not exist, log it up and move on
+      core.debug(`Label ${labelToAdd} does not exist`);
 
       return;
     }
